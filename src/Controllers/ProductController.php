1a2bb5358459b81cc54285cd91f30fed492c73c3
MrPho <nguyentrongpho@gmail.com>
2021-05-30 00:07:04 +0700

    update

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -32,7 +32,7 @@ class ProductController extends Controller {
 		$this->termRepository = $termRepository;
 		$this->postRepository = $postRepository;
 		$this->unitService = $unitService;
-		$this->taxonomy = config('option.taxonomy.productgroup');
+		$this->taxonomy = config('term.taxonomy.productgroup');
 		$this->type = config('option.post_type.product');
 	}
 

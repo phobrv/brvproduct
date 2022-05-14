ba2e455a77bfa6c7d23b4be05af9871371459d8c
Phont <nguyentrongpho@gmail.com>
2022-05-14 22:40:19 +0700

    update

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -12,6 +12,8 @@ use Phobrv\BrvCore\Services\ConfigLangService;
 use Phobrv\BrvCore\Services\UnitServices;
 use Phobrv\BrvCore\Services\VString;
 use Yajra\Datatables\Datatables;
+use Phobrv\BrvCore\Services\PostServices;
+
 
 class ProductController extends Controller
 {
@@ -19,6 +21,7 @@ class ProductController extends Controller
     protected $userRepository;
     protected $termRepository;
     protected $postRepository;
+    protected $postService;
     protected $unitService;
     protected $taxonomy;
     protected $type;
@@ -31,6 +34,7 @@ class ProductController extends Controller
         UserRepository $userRepository,
         TermRepository $termRepository,
         PostRepository $postRepository,
+        PostServices $postService,
         UnitServices $unitService
     ) {
         $this->vstring = $vstring;
@@ -38,6 +42,7 @@ class ProductController extends Controller
         $this->userRepository = $userRepository;
         $this->termRepository = $termRepository;
         $this->postRepository = $postRepository;
+        $this->postService = $postService;
         $this->unitService = $unitService;
         $this->taxonomy = config('term.taxonomy.productgroup');
         $this->type = config('option.post_type.product');
@@ -80,22 +85,22 @@ class ProductController extends Controller
         }
 
         return Datatables::of($data['products'])
-            ->addColumn('title', function ($product) {
-                return view('phobrv::product.components.viewTitle', ['product' => $product]);
-            })
-            ->addColumn('edit', function ($product) {
-                return view('phobrv::product.components.editBtn', ['product' => $product]);
-            })
-            ->addColumn('status', function ($product) {
-                return view('phobrv::product.components.statusLabel', ['product' => $product]);
-            })
-            ->addColumn('langButtons', function ($product) {
-                return view('phobrv::product.components.langButtons', ['buttons' => $product->buttons]);
-            })
-            ->addColumn('delete', function ($product) {
-                return view('phobrv::product.components.deleteBtn', ['product' => $product]);
-            })
-            ->make(true);
+        ->addColumn('title', function ($product) {
+            return view('phobrv::product.components.viewTitle', ['product' => $product]);
+        })
+        ->addColumn('edit', function ($product) {
+            return view('phobrv::product.components.editBtn', ['product' => $product]);
+        })
+        ->addColumn('status', function ($product) {
+            return view('phobrv::product.components.statusLabel', ['product' => $product]);
+        })
+        ->addColumn('langButtons', function ($product) {
+            return view('phobrv::product.components.langButtons', ['buttons' => $product->buttons]);
+        })
+        ->addColumn('delete', function ($product) {
+            return view('phobrv::product.components.deleteBtn', ['product' => $product]);
+        })
+        ->make(true);
     }
 
     public function create()
@@ -166,8 +171,8 @@ class ProductController extends Controller
             $data['post'] = $this->postRepository->find($id);
             $data['boxTranslate'] = $this->configLangService->genLangTranslateBox($data['post']);
             $data['arrayGroupID'] = $this->termRepository->getArrayTermIDByTaxonomy($data['post']->terms, $this->taxonomy);
-            $data['gallery'] = $this->postRepository->getMultiMetaByKey($data['post']->postMetas, "image");
-            $data['meta'] = $this->postRepository->getMeta($data['post']->postMetas);
+            $data['gallery'] = $this->postService->getMultiMetaByKey($data['post']->postMetas, "image");
+            $data['meta'] = $this->postService->getMeta($data['post']->postMetas);
             return view('phobrv::product.edit')->with('data', $data);
         } catch (Exception $e) {
             return back()->with('alert_danger', $e->getMessage());

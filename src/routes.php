d9bbdae4368cffa39b24ea40f6a803213615665e
phont <phont@arrow-tech.vn>
2022-06-08 10:50:30 +0700

    update

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
 Route::middleware(['web', 'auth', 'auth:sanctum', 'lang', 'verified'])->namespace('Phobrv\BrvCore\Http\Controllers')->group(function () {
 	Route::middleware(['can:product_manage'])->prefix('admin')->group(function () {
 		Route::resource('brand', 'TermController');
